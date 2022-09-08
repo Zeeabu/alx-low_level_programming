46ad6a301f585f5b3498a1bf5deca814aa94e441
Zeeabu <Xaynab28@gmail.com>
2022-09-08 16:15:21 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ 
+ * main - Printing
+ 
+ *
+ 
+ * Return: Always 0 (Success)
+ 
+ */
+
+int main(void)
+
+{  	
+     puts("\"Programming is like building a multilingual puzzle");
+     
+         return(0);
+}
