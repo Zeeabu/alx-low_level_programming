2b3dfc8f26d7ac327832ad6f46aedf59e3aefc1c
Zeeabu <Xaynab28@gmail.com>
2022-09-09 14:43:29 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print size is not granduer
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("Size of char: %d byte(s)\n", sizeof(a));
+printf("Size of int: %d byte(s)\n", sizeof(b));
+printf("Size of long int: %d byte(s)\n", sizeof(c));
+printf("Size of long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizaof(e));
+return (0);
+}
