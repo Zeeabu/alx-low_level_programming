54cd086ea7e272c87a75f6638be4447ae28819e7
Zeeabu <Xaynab28@gmail.com>
2022-09-10 11:01:07 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - 5-print_numbers.c
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n')
+return (0);
+}
