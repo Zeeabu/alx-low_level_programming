797a8023a721c522e624bcaa20e0bfb9417fd453
Zeeabu <Xaynab28@gmail.com>
2022-09-10 09:25:18 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Lowercase alphabets
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+puchar(c);
+putchar('\n');
+return (0);
+}
