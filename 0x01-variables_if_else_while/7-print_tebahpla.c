3dd81b30e089a41761e0e1f1b617be28cf6b2ee5
Zeeabu <Xaynab28@gmail.com>
2022-09-11 04:30:54 -0700

    print 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - 7-print_tebahpla.c
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c => 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
