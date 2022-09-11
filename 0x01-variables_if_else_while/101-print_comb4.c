f1b633b421cbad2cd1adf7488444017367f08da1
Zeeabu <Xaynab28@gmail.com>
2022-09-11 07:46:21 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@ int main(void)
 int i, p, m;
 for (i = 0; i <= 9; i++)
 {
-for (p = 1; <= 9; p++)
+for (p = 1; p <= 9; p++)
 {
-for (m = 2; <= 9; m++)
+for (m = 2; m <= 9; m++)
 {
 if (m > p && p > i)
 {
