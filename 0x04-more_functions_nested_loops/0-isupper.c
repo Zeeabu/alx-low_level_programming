cd13dc4028a66bd74008488080a167333f5c1bfd
Zeeabu <Xaynab28@gmail.com>
2022-09-15 14:57:07 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *_isupper - This is a function to check uppercase alphabets
- *@c: integer input
+ *_isupper - checks if a character is uppercase or not
+ *@c: character to be tested
  *Return: 1 whether it is, 0 otherwise
  */
+
 int _isupper(int c)
 {
 if ((c >= 'A') && (c <= 'Z'));
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,14 +2,11 @@
 #define MAIN_H
 
 /**
- *main - headerfile for prototypes
+ *main - header file for prototypes
  *void prototypes (void)
  *int prototypes (int)
  */
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -17,5 +14,8 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);                                                            int mul(int a, int b);
 
 #endif /* MAIN_H */
