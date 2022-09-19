2bd33a3a2d688a96ea166d9f6412bf4c34950211
Zeeabu <Xaynab28@gmail.com>
2022-09-19 09:34:44 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,7 +19,7 @@ sign = 1;
 while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 {
 if (*temp == '-')
-sign *= -1
+sign *= -1;
 temp++;
 }
 if (*temp != '\0')
@@ -31,4 +31,4 @@ temp++;
 while (*temp >= '0' && *temp <= '9');
 }
 return (num * sign);
-
+}
