8e7e482723534e0c59178a8a31863731d38663de
Zeeabu <Xaynab28@gmail.com>
2022-09-19 08:56:57 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,16 @@
 
 void puts2(char *str)
 {
-int i = 0;
-while (*(str + i) != '\0')
+int i;
+i = 0;
+while (str[i] != '\0')
+{
 if (i % 2 == 0)
-putchar(*(str + i));
+{
+_putchar(str[i]);
+}
 i++;
-putchar(10);
+}
+_putchar('\n');
 }
 
