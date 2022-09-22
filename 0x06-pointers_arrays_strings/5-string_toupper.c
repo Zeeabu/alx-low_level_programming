c37545ec2308c76ffc28aca56f01b03c6a09d521
Zeeabu <Xaynab28@gmail.com>
2022-09-22 11:04:15 -0700

    task 5 and 6

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,9 +14,8 @@ char *string_toupper(char *str)
 int index = 0;
 while (str[index++])
 {
-if (str[i] >= 'a' && str[i] <= 'z')
-str[i] -= 32;
-index++;
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
 }
 return (str);
 }
