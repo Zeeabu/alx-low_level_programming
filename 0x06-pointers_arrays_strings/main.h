37bac8bfadc6be6c30db95bd3100f0808b4f9419
Zeeabu <Xaynab28@gmail.com>
2022-09-22 11:09:58 -0700

    task 5 and 6

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,9 +7,8 @@
 *int prototypes()
 *char prototypes()
 */
-
-int main(void)
-void print_array(int *a, int n)
+int main(void);
+void print_array(int *a, int n);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
