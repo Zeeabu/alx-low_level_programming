ac7570667bb7bd7189159015e34736a89331f9ec
Zeeabu <Xaynab28@gmail.com>
2022-09-26 15:19:32 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,18 +11,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int a = 0, b, t = 0;
-{
 while (accept[a])
 {
 b = 0;
 while (s[b] != 32)
+{
 if (accept[a] == s[b])
 {
 t++;
 }
 b++;
 }
-a++
+a++;
 }
 return (t);
 }
