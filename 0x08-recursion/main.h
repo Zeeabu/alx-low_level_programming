8a6111117e43c245499236e03c390a95846cda39
Zeeabu <xaynab28@gmail.com>
2022-09-29 12:41:16 -0700

    task 8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int is_prime(int n, int start);
+void check(char *s, int start, int end, int *flag);
 
 #endif /* MAIN_H */
