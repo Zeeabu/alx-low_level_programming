21644cf50c8fc1b825ca020049dc63a64fad1736
Zeeabu <xaynab28@gmail.com>
2022-09-30 14:51:07 -0700

    task 5

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *main - Aprogram that multiplies two numbers
+ *@argc: count argument
+ *@argv: Argument vector
+ *
+ *Return: 0 if true and 1 if false
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b;
+if (argc == 1)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
+return (0);
+}
+printf("error\n");
+return (1);
+}
