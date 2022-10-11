a37f3fd877d4e79bc19999d4d70957fc0167ac88
Zeeabu <xaynab28@gmail.com>
2022-10-11 13:47:09 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initializes a variable of tpe struct dog
+ *@d: the dog to be initialize
+ *@name: the name of the dog
+ *@age: the age of the dog
+ *@owner: the owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
