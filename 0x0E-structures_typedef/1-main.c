890783f645cc39a84a74dd01d4369184af50cff0
Zeeabu <xaynab28@gmail.com>
2022-10-11 14:01:12 -0700

    task 1

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * main - check the code
+ * main - check the code for alx student
  *
  * Return: Always 0.
  */
@@ -11,7 +11,7 @@ int main(void)
 
 {
 struct dog my_dog;
-init_dog(&my_dog, "Poppy", 3.5, "Bob");
+init_dog(&my_dog, "Tina", 3.5, "Lil");
 printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 return (0);
 }
