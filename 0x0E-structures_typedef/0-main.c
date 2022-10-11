86c7ac3fdbed5375b7315157dcdcd8e29ffe9054
Zeeabu <xaynab28@gmail.com>
2022-10-11 14:24:56 -0700

    task 0

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -11,9 +11,9 @@ int main(void)
 {
 struct dog my_dog;
 
-my_dog.name = "Poppy";
+my_dog.name = "Tina";
 my_dog.age = 3.5;
-my_dog.owner = "Bob";
+my_dog.owner = "Lil";
 printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age)
 return (0);
 }
