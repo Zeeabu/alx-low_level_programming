4d802eb13a8af3f7d8d5dcd976f5efa94e8a0587
Zeeabu <xaynab28@gmail.com>
2022-10-12 15:45:51 -0700

    task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,26 +6,23 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 Success
+ * Return: 0.
  */
 
 int main(int argc, char *argv[])
 {
 int count, bytes;
-
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-
 bytes = atoi(argv[1]);
 if (bytes < 0)
 {
 printf("Error\n");
 exit(2);
 }
-
 for (count = 0; count < bytes; count++)
 {
 printf("%02hhx\n", *((char *)main + count));
