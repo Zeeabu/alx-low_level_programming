f8ead3f526aa84bc856f096410cef722c89faa85
Zeeabu <xaynab28@gmail.com>
2022-10-20 14:06:26 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * hare - Prints a string of words
+ * hare - Prints a string before the
  *         main function is executed
  */
 
 void hare(void)
 {
-	printf("You're beat! and yet, you must allow,"
-		"\nI bore my house upon my back!)\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!)\n");
 }
 
