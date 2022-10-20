362c4cd1aaf2b9417e58e95c19002bd32b06b22d
Zeeabu <xaynab28@gmail.com>
2022-10-20 13:44:45 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * main - Prints a string of words
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+return (0);
+}
+
