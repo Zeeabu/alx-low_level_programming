ef229eff976c38416c0e5466262ff6c5d7b951e4
Zeeabu <xaynab28@gmail.com>
2022-10-25 16:15:01 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,15 +24,15 @@ return (NULL);
 new->n = n;
 if (idx == 0)
 {
-	new->next = copy;
-	*head = new;
-	return (new);
+new->next = copy;
+*head = new;
+return (new);
 }
-	for (node = 0; node < (idx - 1); node++)
+for (node = 0; node < (idx - 1); node++)
 {
-	if (copy == NULL || copy->next == NULL)
-		return (NULL);
-		copy = copy->next;
+while (copy == NULL || copy->next == NULL)
+return (NULL);
+copy = copy->next;
 }
 new->next = copy->next;
 copy->next = new;
