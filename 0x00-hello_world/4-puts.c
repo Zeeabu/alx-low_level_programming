9e7787a2341f15a5724bd9bb98f35dcf5b0a0fe2
Zeeabu <xaynab28@gmail.com>
2022-12-29 15:08:49 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
  #include <stdio.h>
 
- /**
-  * main - 4-puts.c
-  *
-  *Description: using the main function
-  *This program prints "Programming is like a multilingual puzzle
-  * Return: Always 0 (Success)
-  */
+/**
+ *main - 4-puts.c
+ *
+ *Description: using the main function
+ *This program prints "Programming is like a multilingual puzzle
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
-
