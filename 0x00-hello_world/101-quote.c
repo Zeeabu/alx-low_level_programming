31f4bcd43eebb6604ace0e05305a8d1bee11d86f
Zeeabu <xaynab28@gmail.com>
2022-12-29 16:21:59 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Prints a text
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
