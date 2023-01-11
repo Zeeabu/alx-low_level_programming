994dfb69ffcc0dd2d73bd187a21e408f7ab24d7d
Zeeabu <Xaynab28@gmail.com>
2023-01-11 15:03:41 -0800

    hash tables

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return:Always 0 (Success)
+ * Return: 0 
  */
 
 int main(void)
@@ -12,6 +12,11 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
 
 	printf("infinite loop avoided! \\o/\n");
 
